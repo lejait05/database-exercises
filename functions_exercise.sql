2057bb6f1bc0641b506d5e9a16a8138184240c73
Lineisha <lineishagibson@gmail.com>
2022-07-01 16:43:43 -0500

    databases

--- functions_exercise.sql
+++ functions_exercise.sql
@@ -2,4 +2,4 @@ SELECT * FROM employees WHERE last_name LIKE '%E%'ORDER BY emp_no DESC;
 SELECT * FROM employees WHERE last_name like 'E%' OR last_name LIKE  '%E';
 SELECT * FROM employees WHERE last_name like 'E%' AND last_name like '%E';
 SELECT CONCAT(first_name, ' ',last_name) FROM employees WHERE last_name like 'E%' AND last_name like '%E';
-SELECT * FROM employees WHERE year(hire_date) between '1990' AND '1999' AND month( birth_date) = '12' AND day(birth_date) ='25';
+SELECT * FROM employees WHERE year(hire_date) between '1990' AND '1999' AND month( birth_date) = '12' AND day(birth_date) ='25' ORDER BY birth_date, hire_date DESC;
---
+++ group_by.sql
@@ -0,0 +1,6 @@
+USE employees;
+SELECT DISTINCT title FROM titles;
+SELECT first_name,last_name FROM employees WHERE last_name LIKE 'E%'AND last_name LIKE '%E' GROUP BY first_name, last_name;
+SELECT last_name FROM employees WHERE last_name LIKE '%q%'  AND last_name  NOT LIKE  '%qu%' GROUP BY last_name;
+SELECT count(last_name),last_name  FROM employees WHERE last_name LIKE '%q%'  AND last_name  NOT LIKE  '%qu%' GROUP BY last_name ORDER BY last_name;
+SELECT count(*), gender FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya')GROUP BY gender;
